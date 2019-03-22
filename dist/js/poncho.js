598b950db384acb5f4caa3d1af7eac512eb6a986
Leonardo Santa Cruz <lsantacruz@modernizacion.gob.ar>
2019-03-22 12:39:45 -0300

    Actualizacion PONCHO Popover

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -422,3 +422,20 @@ optimized: false
  });
 
 }
+
+
+
+
+
+
+//#####################################################################
+//####################### POPOVER #####################################
+//#####################################################################
+
+var content_popover = document.getElementById('content-popover');
+function popshow(){
+ content_popover.classList.toggle('hidden');
+}
+function pophidde(){
+ content_popover.classList.add('hidden')
+}
