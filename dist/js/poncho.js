23ec9dabaeb42c2ca195395bd24252dba5bd185d
agustin.bouillet <agustin.bouillet@gmail.com>
2022-09-12 11:45:49 -0300

    minify

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -514,33 +514,43 @@ function ponchoChart(opt) {
       var codigoColor = '';
       switch (color) {
           case 'celeste':
+          case 'info':
               codigoColor = '#2897d4';
               break;
           case 'verde':
+          case 'success':
               codigoColor = '#2e7d33';
               break;
           case 'rojo':
+          case 'danger':
               codigoColor = '#c62828';
               break;
           case 'amarillo':
+          case 'warning':
               codigoColor = '#f9a822';
               break;
           case 'azul':
+          case 'info':
               codigoColor = '#0072bb';
               break;
           case 'negro':
+          case 'black':
               codigoColor = '#333';
               break;
           case 'uva':
               codigoColor = '#6a1b99';
               break;
           case 'gris':
+          case 'muted':
               codigoColor = '#525252';
               break;
           case 'grisintermedio':
+          case 'gray':
+          case 'gris-area':
               codigoColor = '#f2f2f2';
               break;
           case 'celesteargentina':
+          case 'celeste-argentina':
               codigoColor = '#37bbed';
               break;
           case 'fucsia':
@@ -548,9 +558,6 @@ function ponchoChart(opt) {
               break;
           case 'arandano':
               codigoColor = '#c2185b';
-              break;
-          case 'uva':
-              codigoColor = '#6a1b99';
               break;
           case 'cielo':
               codigoColor = '#039be5';
@@ -562,7 +569,6 @@ function ponchoChart(opt) {
               codigoColor = '#cddc39';
               break;
           case 'maiz':
-              codigoColor = '#ffce00';
           case 'maíz':
               codigoColor = '#ffce00';
               break;
@@ -570,9 +576,11 @@ function ponchoChart(opt) {
               codigoColor = '#ef5350';
               break;
           case 'naranjaoscuro':
+          case 'naranja':
               codigoColor = '#EF6C00';
               break;
           case 'verdeazulado':
+          case 'verde-azulado':
               codigoColor = '#008388';
               break;
           case 'escarapela':
@@ -594,12 +602,15 @@ function ponchoChart(opt) {
               codigoColor = '#d7df23';
               break;
           case 'verdejade':
+          case 'verde-jade':
               codigoColor = '#066';
               break;
           case 'verdealoe':
+          case "verde-aloe":
               codigoColor = '#4fbb73';
               break;
           case 'verdecemento':
+          case 'verde-cemento':
               codigoColor = '#b4beba';
               break;
           default:
@@ -1512,6 +1523,7 @@ function ponchoChart(opt) {
       }
   }
 }
+
 
 //#####################################################################
 //####################### GAPI LEGACY #################################
