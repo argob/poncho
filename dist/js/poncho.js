e2a785891f79560eb6905be24e45f3a04dffb32a
agustinbouillet <agustinbouillet@users.noreply.github.com>
2023-02-21 12:46:57 +0000

    🧪 Deploy with build-poncho

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -573,9 +573,7 @@ function ponchoTable(opt) {
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-const ponchoTableDependant2 = opt => {
-  // return ponchoTable(opt);
-
+const ponchoTableDependant = opt => {
   var gapi_data;
   var filtersList = [];
   var filtro = {};
