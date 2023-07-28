3018f51e3bae1cae88e7399083a2ee4d57513d1b
Agustín Bouillet <agustinbouillet@Agustins-MacBook-Pro.local>
2023-07-28 12:40:23 -0300

    test

--- test/color.test.js
+++ test/color.test.js
@@ -1,8 +1,16 @@
-const {ponchoColor} = require('../src/js/utils/color');
+const {ponchoColor, findPonchoColorByHex} = require('../src/js/utils/color');
 
 
 test('Colores Poncho por nombre', () => {
     expect(ponchoColor('amarillo')).toBe('#f9a822');
     expect(ponchoColor('warning')).toBe('#f9a822');
     expect(ponchoColor('Esto no es un color')).toBe('Esto no es un color');
+});
+
+
+test("Buscar colores por su valor hexadecimal", () => {
+    expect(findPonchoColorByHex("333").code).toBe("black");
+    expect(findPonchoColorByHex("333333").code).toBe("black");
+    expect(findPonchoColorByHex("#333").code).toBe("black");
+    expect(findPonchoColorByHex(333).code).toBe("black");
 });
