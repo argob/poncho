edf89028cc7e9405ed1b9d8a1c6654919655c167
agustin bouillet <agustin.bouillet@gmail.com>
2024-03-14 15:18:36 -0300

    replace host

--- src/js/device-breadcrumb/src/js/device-breadcrumb.js
+++ src/js/device-breadcrumb/src/js/device-breadcrumb.js
@@ -73,7 +73,7 @@ class DeviceBreadcrumb {
         return {
             breakPoint: 991,
             selector: ".breadcrumb",
-            domain: ["^/$", "argentina.gob.ar$", "argentina.gob.ar/$"]
+            domain: ["^/$",  `${location.host}/?$`]
         };
     };
 
