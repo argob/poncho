c30bc3703faff113a79cf6a168a7a973c5476c79
agustinbouillet <agustinbouillet@users.noreply.github.com>
2024-05-18 20:45:37 +0000

    🧪 Deploy with build-poncho

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -519,6 +519,27 @@ if (typeof exports !== "undefined") {
 }
 
 
+function flattenNestedObjects(entries) {
+    return entries.map(entry => {
+        return flattenObject(entry, "");
+    });
+}
+
+function flattenObject(obj, prefix) {
+    const flattened = {};
+    for (const key in obj) {
+        const value = obj[key];
+        const newKey = (prefix ? `${prefix}__${key}` : key);
+
+        if (typeof value === "object" && value !== null) {
+            Object.assign(flattened, flattenObject(value, newKey));
+        } else {
+            flattened[newKey] = value;
+        }
+    }
+    return flattened;
+}
+
 /**
  * 
  */
