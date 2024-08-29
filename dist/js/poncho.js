b22981eafa9fc01f928c41521b96b96f15f22ad8
agustinbouillet <agustinbouillet@users.noreply.github.com>
2024-08-29 18:32:11 +0000

    🧪 Deploy with build-poncho

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -2170,10 +2170,26 @@ const ponchoTableDependant = opt => {
     function _eventDispatcher(selector, value, eventType){
         const element = document.querySelectorAll(`#${selector}`);
         element.forEach(ele => {
-            ele.value = value;
-            const event = new Event(eventType);
-            ele.dispatchEvent(event);
+            setTimeout(function(){
+                ele.value = value;
+                const event = new Event(eventType);
+                ele.dispatchEvent(event);
+            },50)
         });
+    }
+
+
+    /**
+     * window pushState
+     * 
+     * @param {string} url Url
+     * @returns {undefined}
+     */
+    function _pushState(url){
+        if (opt.hasOwnProperty("pushState") && 
+            typeof opt.pushState === "boolean" && opt.pushState) {
+            window.history.pushState({}, "", url);
+        }
     }
 
 
@@ -2228,6 +2244,8 @@ const ponchoTableDependant = opt => {
                 e.innerHTML = url.href;
             }
         });
+
+        _pushState(url.href);
     }
 
 
