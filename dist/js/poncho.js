fff09e456237d4b6329bc44c15bd2a070cfec62d
agustinbouillet <agustinbouillet@users.noreply.github.com>
2024-12-12 16:23:44 +0000

    🧪 Deploy with build-poncho

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -42,6 +42,7 @@ if (typeof exports !== "undefined") {
  *
  * Copyright (c) 2024 Argentina.gob.ar
  *
+ * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction,
