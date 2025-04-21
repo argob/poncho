074ee140f650b5fe3d57b5e7aff6a47f9a1d9c1d
agustinbouillet <agustinbouillet@users.noreply.github.com>
2025-04-21 18:20:01 +0000

    🧪 Deploy with build-poncho

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -3229,16 +3229,12 @@ var ponchoUbicacion = function(options) {
     * @returns 
     */
     function parseJsonMunicipios(data) {
-        const groupedData = data.municipios.reduce((acc, current) => {
-            const key = `${current.nombre}`;
-            current.label = key;
-            if (!acc[key]) {
-                acc[key] = current;
-            }
-            return acc;
-        }, {});
-
-    return Object.values(groupedData);
+        return data.municipios.map(municipio => {
+            return {
+                ...municipio,
+                label: municipio.nombre_completo
+            };
+        });
     }
     
     /**
@@ -3247,15 +3243,12 @@ var ponchoUbicacion = function(options) {
      * @returns 
      */
     function parseJsonLocalidades(data) {
-        const groupedData = data.localidades.reduce((acc, current) => {
-            const key = `${current.departamento.nombre} - ${current.nombre}`;
-            current.label = key;
-            if (!acc[key]) {
-                acc[key] = current;
-            }
-            return acc;
-        }, {});
-        return Object.values(groupedData);
+        return data.localidades.map(localidad => {
+            return {
+                ...localidad,
+                label: `${localidad.departamento.nombre} - ${localidad.nombre}`
+            };
+        });
     }
 
 
@@ -3376,7 +3369,6 @@ var ponchoUbicacion = function(options) {
     * @returns 
     */
     function getSelectMunicipios(municipios, provincia) {
-        debugger
         var muniSelect = {};
         var required = iMunicipio.prop('required');
         var select = null;
@@ -3429,8 +3421,7 @@ var ponchoUbicacion = function(options) {
                     return nameA.localeCompare(nameB);
                 });
             
-            emptyOption = (iLocalidad.val() ? true : false);
-            select = getDropDownList(
+             select = getDropDownList(
                 'sLocalidades', 'sLocalidades',
                 locaSelect, required, emptyOption, iLocalidad.val()
             );
@@ -3446,6 +3437,7 @@ var ponchoUbicacion = function(options) {
 
     init();
 };
+
 
 /**
  * PONCHO CHART
