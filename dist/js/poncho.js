ca3685d1b67b5908f78d523174d1e36a44089042
agustinbouillet <agustinbouillet@users.noreply.github.com>
2025-12-09 16:52:23 +0000

    🧪 Deploy with build-poncho

--- dist/js/poncho.js
+++ dist/js/poncho.js
@@ -9029,8 +9029,9 @@ class PonchoMap {
             element.setAttribute("aria-labelledby", id);
         }
 
-        // Creo el elemento
-        const p = document.createElement("p");
+        // Si existe el elemento lo uso. De otro modo lo creo.
+        const p = (document.querySelector(`#${id}`) || 
+                document.createElement("p"));
         p.textContent = summary;
         p.id = id;
         p.classList.add("pm-summary");
